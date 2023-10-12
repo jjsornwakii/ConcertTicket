acdea85665b47708943017f47f24e32897dd619a
JJ <80116206+jjsornwakii@users.noreply.github.com>
2023-10-12 21:29:32 +0700

    update DB

--- src/DB.tsx
+++ src/DB.tsx
@@ -1,2 +1,1 @@
-// export const dbURL = "https://project-2exq5ozz4-shidkung.vercel.app/"
-export const dbURL = "http://localhost:3000/"
+export const dbURL = "https://project-2exq5ozz4-shidkung.vercel.app/"
