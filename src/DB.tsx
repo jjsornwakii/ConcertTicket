4b82f763e29f7b3362022e6bb5efa305cac20b5c
64010850 Siwakorn Suriya <80116206+jjsornwakii@users.noreply.github.com>
2023-10-12 20:59:07 +0700

    Revert "Revert "FE:layout Noti, apply font""

--- src/DB.tsx
+++ src/DB.tsx
@@ -1,1 +1,2 @@
-export const dbURL = "https://project-2exq5ozz4-shidkung.vercel.app/"
+// export const dbURL = "https://project-2exq5ozz4-shidkung.vercel.app/"
+export const dbURL = "http://localhost:3000/"
