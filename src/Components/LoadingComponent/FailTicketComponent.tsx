e1aace6872e021603108132abd8ffb147eaa9b10
Burit S <burit3594@gmail.com>
2023-10-12 21:05:29 +0700

    Fix

--- src/Components/LoadingComponent/FailTicketComponent.tsx
+++ src/Components/LoadingComponent/FailTicketComponent.tsx
@@ -18,7 +18,7 @@ const FailTicketComponent: React.FC<LoadingComponentProps> = ({ isLoading }) => 
             <h2>เสียใจด้วย บัตรหมดแล้ว</h2>
             <button className='button-home'>
               {/* TO DO change link to Home please //จากมะม่วง */}
-              <Link to="http://localhost:3001/">
+              <Link to="/">
                 <div className='button-home__text'>
                   กลับไปหน้าหลัก
                 </div>
